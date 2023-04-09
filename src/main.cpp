f86d9e622d6278a853eb41902b2542f572e61678
SpeccyPlays <102996742+SpeccyPlays@users.noreply.github.com>
2023-04-09 16:43:11 +0100

    Started adding opcodes

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,10 @@ class CPU {
   uint8_t RAM[4096]; //putting here for the moment
   void run(){
     //main running loop
+    if (refreshTimer){
+      updateTimers(delayTimer);
+      updateTimers(soundTimer);
+    }
   }
   uint16_t fetch(){
     //read instructions from memory - 2 bytes
@@ -30,21 +34,54 @@ class CPU {
 
     return instruction;
   }
-  void decode(uint16_t fetchedValue){
-    //each of the nibbles mean different things
+  void execute(uint16_t fetchedValue){
+        //each of the nibbles mean different things
     uint16_t addr = fetchedValue & 0xC; //we want lowest 12 bits only
     uint8_t n = fetchedValue & 0xF; //lowest 4 bits
     uint8_t x = (fetchedValue & 0xF00) >> 8; //lowest 4 bits of high byte
-    uint8_t y = (fetchedValue & 0xF0) >> 4; //highest 4 bits of low byte
+    uint8_t y = (fetchedValue & 0x00F0) >> 4; //highest 4 bits of low byte
     uint8_t k = fetchedValue & 0xFF; //lowest 8 bits of instruction
-
-  }
-  void execute(){
-    if (refreshTimer){
-      updateTimers(delayTimer);
-      updateTimers(soundTimer);
+    switch(fetchedValue & 0xF000){ //not sure about this. Used https://www.freecodecamp.org/news/creating-your-very-own-chip-8-emulator/
+      case 0x0000:
+        switch (fetchedValue){
+          case 0x00E0: //CLS
+            //clears screen
+            break;
+          case 0x00EE: //RET
+            //return from subroutine
+            break;
+        }
+      case 0x1000:
+        break;
+      case 0x2000:
+        break;
+      case 0x3000:
+        break;
+      case 0x4000:
+        break;
+      case 0x5000:
+        break;
+      case 0x6000:
+        break;
+      case 0x7000:
+        break;
+      case 0x8000:
+        break;
+      case 0x9000:
+        break;
+      case 0xA000:
+        break;
+      case 0xB000:
+        break;
+      case 0xC000:
+        break;
+      case 0xD000:
+        break;
+      case 0xE000:
+        break;
+      case 0xF000:
+        break;
     }
-
   }
   void push(uint8_t &value){
     //to push on to stack
@@ -52,10 +89,12 @@ class CPU {
     stack[sp] = value;
     sp ++;
   }
-  void pop(){
+  uint8_t pop(){
     //to pop from the stack
+    uint8_t returnValue = stack[sp];
     stack[sp] = 0;
     sp --;
+    return returnValue;
   }
   bool refreshTimer(){
     // check if we're
