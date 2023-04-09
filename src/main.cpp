38f254753aaab0fc741ae2a347ab12697ae27899
SpeccyPlays <102996742+SpeccyPlays@users.noreply.github.com>
2023-04-09 17:22:03 +0100

    Skelton outline for all op codes added

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ class CPU {
   uint8_t soundTimer; //functions the same as delay timers but beeps always unless 0
   uint16_t cycleCounter; //
   float refreshRate = 16.67;
-  uint8_t v[16]; //V0, V1, V2, V3, V4, V5, V6, V7, V8, V9 ,VA, VB, VC, VD, VE; general purpose variable registers 
-  uint8_t VF; //general purpose but also used as a flag register
+  uint8_t v[16]; //V0, V1, V2, V3, V4, V5, V6, V7, V8, V9 ,VA, VB, VC, VD, VE, VF; general purpose variable registers 
+  // VF is general purpose but also used as a flag register
   //the memory
   uint8_t RAM[4096]; //putting here for the moment
   void run(){
@@ -40,7 +40,7 @@ class CPU {
     uint8_t n = fetchedValue & 0xF; //lowest 4 bits
     uint8_t x = (fetchedValue & 0xF00) >> 8; //lowest 4 bits of high byte
     uint8_t y = (fetchedValue & 0x00F0) >> 4; //highest 4 bits of low byte
-    uint8_t k = fetchedValue & 0xFF; //lowest 8 bits of instruction
+    uint8_t kk = fetchedValue & 0xFF; //lowest 8 bits of instruction
     switch(fetchedValue & 0xF000){ //not sure about this. Used https://www.freecodecamp.org/news/creating-your-very-own-chip-8-emulator/
       case 0x0000:
         switch (fetchedValue){
@@ -51,35 +51,81 @@ class CPU {
             //return from subroutine
             break;
         }
-      case 0x1000:
+      case 0x1000: //JP addr
         break;
-      case 0x2000:
+      case 0x2000: //call addr
         break;
-      case 0x3000:
+      case 0x3000: //SE Vx, byte - Skip next instruction if Vx = kk
         break;
-      case 0x4000:
+      case 0x4000: //SNE Vx, byte - Skip next instruction if Vx != kk
         break;
-      case 0x5000:
+      case 0x5000: //SE Vx, Vy - Skip next instruction if Vx = Vy
         break;
-      case 0x6000:
+      case 0x6000: //LD Vx, byte - Set Vx = kk
         break;
-      case 0x7000:
+      case 0x7000: //7xkk - ADD Vx, byte Set Vx = Vx + kk
         break;
       case 0x8000:
+        switch(n){ //think we decide based on lowest 4 bits
+          case 0x0: //LD Vx, Vy - Set Vx = Vy
+            break;
+          case 0x1: //OR Vx, Vy - Set Vx = Vx OR Vy
+            break;
+          case 0x2: //AND Vx, Vy - Set Vx = Vx AND Vy
+            break;
+          case 0x3: //XOR Vx, Vy - Set Vx = Vx XOR Vy
+            break;
+          case 0x4: //ADD Vx, Vy - Set Vx = Vx + Vy, set VF = carry
+            break;
+          case 0x5: //SUB Vx, Vy - Set Vx = Vx - Vy, set VF = NOT borrow
+            break;
+          case 0x6: //SHR Vx {, Vy} - Set Vx = Vx SHR 1
+            break;
+          case 0x7: //SUBN Vx, Vy - Set Vx = Vy - Vx, set VF = NOT borrow.
+            break;
+          case 0xE: //SHL Vx {, Vy - Set Vx = Vx SHL 1
+            break;
+        }
         break;
-      case 0x9000:
+      case 0x9000: //SNE Vx, Vy - Skip next instruction if Vx != Vy
         break;
-      case 0xA000:
+      case 0xA000: //LD I, addr - Set I = nnn
         break;
-      case 0xB000:
+      case 0xB000: //JP V0, addr - Jump to location nnn + V0
         break;
-      case 0xC000:
+      case 0xC000: //RND Vx, byte - Set Vx = random byte AND kk
         break;
-      case 0xD000:
+      case 0xD000: //DRW Vx, Vy, nibble - Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
         break;
       case 0xE000:
+        switch(kk){
+          case 0x9E: //SKP Vx - Skip next instruction if key with the value of Vx is pressed
+            break;
+          case 0x91: //SKNP Vx - Skip next instruction if key with the value of Vx is not pressed
+            break;
+        }
         break;
       case 0xF000:
+        switch (kk) {
+          case 0x07: //LD Vx, DT - Set Vx = delay timer value
+            break;
+          case 0x0A: //LD Vx, K - Wait for a key press, store the value of the key in Vx
+            break;
+          case 0x15: //LD DT, Vx - Set delay timer = Vx
+            break;
+          case 0x18: //LD ST, Vx - Set sound timer = Vx
+            break;
+          case 0x1E: //ADD I, Vx - Set I = I + Vx
+            break;
+          case 0x29: //LD F, Vx - Set I = location of sprite for digit Vx
+            break;
+          case 0x33: //LD B, Vx - Store BCD representation of Vx in memory locations I, I+1, and I+2
+            break;
+          case 0x55: //LD [I], Vx - Store registers V0 through Vx in memory starting at location I
+            break;
+          case 0x65: //LD Vx, [I] - Read registers V0 through Vx from memory starting at location I
+            break;
+        }
         break;
     }
   }
